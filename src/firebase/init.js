e5c78c821c1602efd8b3888a0807f7f0cb7eea69
usafder <36886437+usafder@users.noreply.github.com>
2021-07-11 16:28:52 +0500

    Updated firebase config

--- src/firebase/init.js
+++ src/firebase/init.js
@@ -1,12 +1,13 @@
-import firebase from 'firebase'
+import firebase from 'firebase';
 
 const firebaseConfig = {
-  apiKey: "your_key",
-  authDomain: "your_project.firebaseapp.com",
-  databaseURL: "https://your_project.firebaseio.com",
-  projectId: "your_project",
-  storageBucket: "your_project.appspot.com",
-  messagingSenderId: "your_message_sending_id",
+  apiKey: process.env.VUE_APP_API_KEY,
+  authDomain: process.env.VUE_APP_AUTH_DOMAIN,
+  databaseURL: process.env.VUE_APP_DB_URL,
+  projectId: process.env.VUE_APP_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_MESSAGING_SENDER_ID,
+  appId: process.env.VUE_APP_ID,
 };
 
 const firebaseInstance = firebase.initializeApp(firebaseConfig);
